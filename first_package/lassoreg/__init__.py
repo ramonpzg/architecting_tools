dda692bfa987f8fc88e1ceccfcccc316ec4bd43f
Ramon Perez <ramonpzg@protonmail.com>
2023-12-07 23:36:19 +0000

    updated pyproject toml

--- first_package/lassoreg/__init__.py
+++ first_package/lassoreg/__init__.py
@@ -1,7 +1,1 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("epi_models")
-except PackageNotFoundError:
-    # If the package is not installed, don't add __version__
-    pass
+__version__ = "0.1.0"
